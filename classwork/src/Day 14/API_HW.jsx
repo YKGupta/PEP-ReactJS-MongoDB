2bc8ae3accdc66b4fb121ad37cd67dff413a8a84
Yash Kumar Gupta <ykgupta2411@gmail.com>
2025-02-06 10:06:51 +0530

    Day 14- Homework

--- classwork/src/Day 14/API_HW.jsx
+++ classwork/src/Day 14/API_HW.jsx
@@ -1,30 +1,34 @@
 import { useState, useEffect } from "react";
 
-const API = () => {
+const API_HW = () => {
     const [resourceType, setresourceType] = useState("posts");
-    const [items, setItems] = useState([]);
 
     useEffect(() => {
-        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+        fetch(`https://jsonplaceholder.typicode.com/${resourceType}`, {
+            method: "POST",
+
+            body: JSON.stringify({
+                title: "Hello",
+                body: "Bye",
+            }),
+
+            headers: {
+                "content-type": "application/json",
+            },
+        })
             .then((response) => response.json())
-            .then((data) => setItems(data))
+            .then((data) => console.log(data))
             .catch((error) => console.log(error));
     }, [resourceType]);
+
     return (
         <div>
             <button onClick={() => setresourceType("posts")}>Get Posts</button>
             <button onClick={() => setresourceType("comments")}>Get Comments</button>
             <button onClick={() => setresourceType("users")}>Get Users</button>
-            <p>You are viewing {resourceType}</p>
-            <ul>
-                {
-                    items.map((val, ind) => {
-                        return <li key={ind}>{JSON.stringify(val)}</li>;
-                    })
-                }
-            </ul>
+            <h2>You clicked {resourceType}</h2>
         </div>
     );
-}
+};
 
-export default API;
+export default API_HW;
